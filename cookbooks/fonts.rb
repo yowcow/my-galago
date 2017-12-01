14233bd0cea58391d3ea05bf7657dc169d16566a
yowcow <yowcow@cpan.org>
2017-12-01 22:52:33 +0900

    http_request -> curl

--- cookbooks/fonts.rb
+++ cookbooks/fonts.rb
@@ -1,11 +1,19 @@
-http_request "/usr/local/share/fonts/source-code-pro.tar.gz" do
-  url "https://github.com/adobe-fonts/source-code-pro/archive/2.030R-ro/1.050R-it.tar.gz"
-  not_if "/usr/local/share/fonts/source-code-pro.tar.gz"
+execute "Download tarball" do
+  command <<-CMD
+    curl -L https://github.com/adobe-fonts/source-code-pro/archive/2.030R-ro/1.050R-it.tar.gz -o /usr/local/share/fonts/source-code-pro.tar.gz
+  CMD
+  not_if "test -e /usr/local/share/fonts/source-code-pro.tar.gz"
 end
 
-execute "Unarchive and refresh font-config" do
+execute "Unarchive tarball" do
   command <<-CMD
-    tar xzf /usr/local/share/fonts/source-code-pro.tar.gz -C /usr/local/share/fonts && fc-cache -f -v
+    tar xzf /usr/local/share/fonts/source-code-pro.tar.gz -C /usr/local/share/fonts
   CMD
   not_if "test -e /usr/local/share/fonts/source-code-pro-2.030R-ro-1.050R-it"
+end
+
+execute "fc-cache" do
+  command <<-CMD
+    fc-cache -f -v
+  CMD
 end
