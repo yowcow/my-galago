27103176dc214814f13a4de3b34c935ac49c4262
yowcow <yowcow@cpan.org>
2017-12-03 12:56:20 +0900

    overpass

--- cookbooks/fonts.rb
+++ cookbooks/fonts.rb
@@ -1,16 +1,5 @@
-execute "Download tarball" do
-  command <<-CMD
-    curl -L https://github.com/adobe-fonts/source-code-pro/archive/2.030R-ro/1.050R-it.tar.gz -o /usr/local/share/fonts/source-code-pro.tar.gz
-  CMD
-  not_if "test -e /usr/local/share/fonts/source-code-pro.tar.gz"
-end
-
-execute "Unarchive tarball" do
-  command <<-CMD
-    tar xzf /usr/local/share/fonts/source-code-pro.tar.gz -C /usr/local/share/fonts
-  CMD
-  not_if "test -e /usr/local/share/fonts/source-code-pro-2.030R-ro-1.050R-it"
-end
+include_recipe "fonts/source-code-pro.rb"
+include_recipe "fonts/overpass.rb"
 
 execute "fc-cache" do
   command <<-CMD
